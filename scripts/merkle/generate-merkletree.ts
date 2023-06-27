cb9805ec60e0bca2a3d978050827b076115af733
fqcdx <fangqicheng@dxchain.com>
2023-06-27 10:22:09 +0800

    feat: implement Merkle tree generation

---
+++ scripts/merkle/generate-merkletree.ts
@@ -0,0 +1,47 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import fs from "fs";
+
+import readline from "readline";
+
+// read csv file and return array of array
+async function readCSVToArray(file_path: string): Promise<string[][]> {
+  const data: string[][] = [];
+
+  const stream = fs.createReadStream(file_path, "utf-8");
+  const rl = readline.createInterface({ input: stream });
+
+  for await (const line of rl) {
+    const values = line
+      .split(",")
+      .map((value) => value.trim().replace(/"/g, ""));
+    data.push(values);
+  }
+
+  return data;
+}
+// generate merkle tree from csv file
+async function generateTree() {
+  const filePath = "scripts/merkle/whitelist.csv";
+  const data = await readCSVToArray(filePath);
+  console.log(data);
+  // ======== Generate Merkle Tree ========
+
+  const tree = StandardMerkleTree.of(data, ["address", "uint256"]);
+  console.log("Merkle Root:", tree.root);
+  fs.writeFileSync("scripts/merkle/tree.json", JSON.stringify(tree.dump()));
+  return tree;
+}
+// main function
+async function main() {
+  // ======== Generate Merkle Proof ========
+  const tree = await generateTree();
+  for (const [i, v] of tree.entries()) {
+    const proof = tree.getProof(i);
+    const root = tree.root;
+    console.log("Index:", i);
+    console.log("Value:", v);
+    console.log("Proof:", proof);
+    console.log("Root:", root);
+  }
+}
+main();
