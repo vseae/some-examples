dee05bd78015d23621f5bf0b4a259c1af710ba64
fqcdx <fangqicheng@dxchain.com>
2023-09-13 15:50:08 +0800

    update

--- deploy/000-Demo.ts
+++ deploy/000-Demo.ts
@@ -1,15 +1,30 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
-  await deploy("Demo", {
+  const DemoArtifact = await deployments.getArtifact("Demo");
+  const res = await deploy("Demo", {
     from: deployer,
     args: [],
     log: true,
     deterministicDeployment: false,
+    proxy: {
+      owner: deployer,
+      proxyContract: "OpenZeppelinTransparentProxy",
+    },
   });
+
+  const demo = await ethers.getContractAt(DemoArtifact.abi, res.address);
+
+  if ((await demo.name()) === "Demo") {
+    console.log("Demo contract already initialized:", await demo.getAddress());
+  } else {
+    const tx = await demo.initialize("Demo", "DEMO");
+    await tx.wait();
+  }
 };
 deploy.tags = ["Demo"];
 export default deploy;
